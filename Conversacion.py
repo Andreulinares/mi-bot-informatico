2b98956006a529655a008aa3c3b05b30909d8989
Andreulinares <145133414+Andreulinares@users.noreply.github.com>
2026-09-06 16:20:27 +0000

    cambios

--- Conversacion.py
+++ Conversacion.py
@@ -6,23 +6,34 @@ from google.genai import types
 
 st.title("Mi bot informatico")
 
-load_dotenv()
-clave = os.getenv("GEMINI_API_KEY")
 
+load_dotenv()
+
+
+if "GEMINI_API_KEY" in st.secrets:
+    clave = st.secrets["GEMINI_API_KEY"]
+else:
+    clave = os.getenv("GEMINI_API_KEY")
 
 @st.cache_resource
-def obtener_cliente():
-    return genai.Client(api_key=clave)
+def obtener_cliente(api_key):
+    
+    return genai.Client(api_key=api_key)
 
 
-cliente = obtener_cliente()
+if not clave:
+    st.error("🔑 Falta la API Key de Gemini. Configúrala en los Secrets de Streamlit.")
+    st.stop()
+
+cliente = obtener_cliente(clave)
 
 if "historial" not in st.session_state:
     st.session_state.historial = []
 
-instruccion = "Eres un expero en informatica. Responde con entusiasmo, menciona datos informaticos cada vez que " \
-              "puedas y a veces suelta expresiones divertidas."
+instruccion = ("Eres un experto en informatica. Responde con entusiasmo, menciona datos informaticos "
+               "cada vez que puedas y a veces suelta expresiones divertidas.")
 
+# Dibujar el historial en pantalla
 for mensaje in st.session_state.historial:
     rol = "user" if mensaje["role"] == "user" else "assistant"
     with st.chat_message(rol):
@@ -39,17 +50,23 @@ if mensaje_usuario := st.chat_input("Escribe tu mensaje"):
 
     with st.chat_message("assistant"):
         with st.spinner("Procesando bits..."):
-            respuesta = cliente.models.generate_content(
-                model="gemini-3.6-flash",
-                contents=st.session_state.historial,
-                config=types.GenerateContentConfig(system_instruction=instruccion,
-                                                   temperature=0.7,
-                                                   max_output_tokens=1000)
-
-            )
-            st.markdown(respuesta.text)
-
-    st.session_state.historial.append({
-        "role": "model",
-        "parts": [{"text": respuesta.text}]
-    })
+            try:
+                
+                respuesta = cliente.models.generate_content(
+                    model="gemini-2.5-flash",
+                    contents=st.session_state.historial,
+                    config=types.GenerateContentConfig(
+                        system_instruction=instruccion,
+                        temperature=0.7,
+                        max_output_tokens=1000
+                    )
+                )
+                st.markdown(respuesta.text)
+                
+                # Guardamos la respuesta exitosa en el historial
+                st.session_state.historial.append({
+                    "role": "model",
+                    "parts": [{"text": respuesta.text}]
+                })
+            except Exception as e:
+                st.error(f"Error al generar contenido: {e}")
